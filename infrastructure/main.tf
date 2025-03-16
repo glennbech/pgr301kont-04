2cad285b09445a0c613908c50c845e85313fa3c4
glennbech <glenn.bech@gmail.com>
2025-03-16 10:12:57 +0100

    remove resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,33 +11,6 @@ provider "statuscake" {
   api_token = var.statuscake_api_token
 }
 
-resource "statuscake_contact_group" "monitoring_group" {
-  name            = "PGR301 Monitoring Group"
-  email_addresses = var.alert_emails
-}
-
-resource "statuscake_uptime_check" "website" {
-  name           = var.website_name
-  check_interval = var.check_interval
-  confirmation   = var.confirmation_count
-  trigger_rate   = var.trigger_rate
-
-  http_check {
-    timeout          = var.timeout
-    validate_ssl     = true
-    follow_redirects = true
-    status_codes     = var.status_codes
-    request_method   = "HTTP"
-  }
-
-  monitored_resource {
-    address = var.website_url
-  }
-
-  contact_groups = [statuscake_contact_group.monitoring_group.id]
-  tags           = var.monitoring_tags
-}
-
 module "prod_monitoring" {
   source = "./modules/monitoring"
 
